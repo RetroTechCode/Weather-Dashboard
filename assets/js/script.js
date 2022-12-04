65432e74c1d78ed7f5034322932008e8867251d9
Dan <danaaboston@gmail.com>
2022-12-03 23:56:03 -0500

    Change structure of functions within js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,19 +13,30 @@ var searchBtn = document.getElementById("searchBtn");
 function search() {
     var input = document.getElementById("input").value;
     console.log(input);
-    currentDay(input);
+    getWeatherData(input);
 };
+
+function getWeatherData(input) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + input + "&units=imperial&appid=" + key)
+    .then((response) => response.json())
+    .then(function(data) {
+        console.log(data);
+        currentDay(data);
+        fiveDay(data);
+    })
+}
 
 // TODO: Function to store user searches to localStorage and display the latest 5 on the page
 
 // TODO: Function to display current day forecast
-function currentDay(input) {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=" + key)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+function currentDay(data) {
+    console.log("Current Day Function");
 }
 
 // TODO: Function to display 5-day forecast
+function fiveDay(data) {
+    console.log("Five Day Function")
+}
 
 // Event listeners
 searchBtn.addEventListener("click", search);
